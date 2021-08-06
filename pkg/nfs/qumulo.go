42c1d438f2e4af2e91ad79ad181146aba90543d2
Travis Cunningham <tcunningham@qumulo.com>
2021-08-06 10:41:52 -0700

    Add basic Qumulo API wrapper

--- pkg/nfs/qumulo.go
+++ pkg/nfs/qumulo.go
@@ -2,6 +2,9 @@ package nfs
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
+	"log"
 )
 
 type Cloud interface {
@@ -9,14 +12,26 @@ type Cloud interface {
 }
 
 type cloud struct {
+	BearerToken string
+}
+
+type BearerTokenResponse struct {
+    BearerToken string
 }
 
 // NewCloud returns a new instance of Qumulo API
 // It panics if session is invalid
 func NewCloud() (Cloud, error) {
 	//TODO read API keys and instantiate new HTTP client here
+	url := "https://10.116.100.111:28615/api"
+
+	authUrl := url + "/v1/session/login"
+
+	bearerTokenResponse := new(BearerTokenResponse)
+	postJsonAuth(authUrl, bearerTokenResponse)
+
 	return &cloud{
-		//client: httpClient
+		BearerToken: bearerTokenResponse.BearerToken,
 	}, nil
 }
 
@@ -26,7 +41,39 @@ type SnapshotInfo struct {
 
 func (c *cloud) CreateSnapshot(ctx context.Context, volumeName string) (snapshotInfo *SnapshotInfo, err error) {
 	// TODO Perform HTTP call here
+
+	url := "https://10.116.100.111:28615/api/v1/snapshots"
+
+    var bearer = "Bearer " + c.BearerToken
+
+    // Create a new request using http
+    req, err := http.NewRequest("POST", url, nil)
+
+    // add authorization header to the req
+    req.Header.Add("Authorization", bearer)
+
+    // Send req using http Client
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        log.Println("Error on response.\n[ERROR] -", err)
+    }
+    defer resp.Body.Close()
+
+	//TODO read response into struct
+
 	return &SnapshotInfo{
 		SnapshotName: "get info from HTTP call here",
 	}, nil
+}
+
+func postJsonAuth(url string, target interface{}) error {
+	req, err := http.NewRequest("POST", url, nil)
+	client := &http.Client{}
+    r, err := client.Do(req)
+    if err != nil {
+        return err
+    }
+    defer r.Body.Close()
+    return json.NewDecoder(r.Body).Decode(target)
 }
